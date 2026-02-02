56a0f98c3313741b2aa86d1d8a2b417c2acc0740
jaden <everywhereworkplace@gmail.com>
2026-02-02 18:47:02 +0900

    빈파일 생성

--- backend/src/main/java/com/dnproject/platform/repository/AdoptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dnproject.platform.repository;
-
-import com.dnproject.platform.domain.Adoption;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdoptionRepository extends JpaRepository<Adoption, Long> {
-}
